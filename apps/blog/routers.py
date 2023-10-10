2bb917d959e78da85be92624ee305b359c96e477
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-10-10 12:16:34 +0330

    adding update and delete

--- apps/blog/routers.py
+++ apps/blog/routers.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
-from .models import Blog
+from fastapi import APIRouter, HTTPException, Response, status
 
+from .models import Blog
 
 router = APIRouter()
 
@@ -35,12 +35,18 @@ def find_blog_by_id(id: int):
             return blog
 
 
+def find_blog_index_by_id(id: int):
+    for index, blog in enumerate(my_blogs):
+        if blog["id"] == id:
+            return index
+
+
 @router.get("/blogs")
 def get_blogs():
     return {"data": my_blogs}
 
 
-@router.post("/blogs")
+@router.post("/blogs", status_code=status.HTTP_201_CREATED)
 def create_blog(blog: Blog):
     new_id = my_blogs[-1]["id"] + 1
     post_dict = blog.model_dump()
@@ -52,4 +58,26 @@ def create_blog(blog: Blog):
 @router.get("/blogs/{id}")
 def get_blog(id: int):
     blog = find_blog_by_id(id)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exists.")
     return {"data": blog}
+
+
+@router.delete("/blogs/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int):
+    blog_index = find_blog_index_by_id(id)
+    if blog_index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exists.")
+    my_blogs.pop(blog_index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/blogs/{id}")
+def update_post(id: int, blog: Blog):
+    blog_index = find_blog_index_by_id(id)
+    if blog_index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exists.")
+    blog_dict = blog.model_dump()
+    blog_dict["id"] = id
+    my_blogs[blog_index] = blog_dict
+    return {"data": blog_dict}
