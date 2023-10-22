9be7f197669521d687af35f8e7cb214c426dfd40
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-10-22 10:21:06 +0330

    Setting up SQLAlchemy

---
+++ apps/core/models.py
@@ -0,0 +1,14 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+
+class Blog(Base):
+    __tablename__ = "blogs"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    is_published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("NOW()"), nullable=False)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
 from apps.blog import routers as BlogRoutes
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from apps.core import models, database
+
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
@@ -7,6 +11,11 @@ app = FastAPI()
 @app.get("/", tags=["Root"])
 def read_root():
     return {"message": "This is Root"}
+
+
+@app.get("/sqlalchemy")
+def test_root(db: Session = Depends(database.get_db)):
+    return {"status": "OK"}
 
 
 app.include_router(BlogRoutes.router, tags=["Blog"])
