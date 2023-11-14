4558450ba43b60951ffcaafff70a28cd399eb8ac
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-11-14 16:50:52 +0330

    Verfying JWT Token

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,6 +1,16 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
--- utils/oauth2.py
+++ utils/oauth2.py
@@ -1,6 +1,11 @@
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWSError, jwt
 from core import config
 from datetime import datetime, timedelta
+from schemas import auth
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def create_access_token(data: dict):
@@ -9,3 +14,19 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, config.SECRET_KEY, config.ALGORITHM)
+        id: str = payload.get("user_id")
+        if not id:
+            raise credentials_exception
+        token_data = auth.TokenData(id=id)
+    except JWSError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_schema)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not Validate Credentials", headers={"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token, credentials_exception)
