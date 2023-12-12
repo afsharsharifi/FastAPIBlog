414fc0b55ea392bf739db03ec243e98811653624
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-12-12 13:58:51 +0330

    Adding Invalid User Test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -18,3 +18,23 @@ def test_login_user(client, test_user):
     assert res.status_code == 200
     assert login_res.token_type == "bearer"
     assert id == test_user["id"]
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "12345678", 403),
+        ("afsharsharifi6@gmail.com", "wrong_password", 403),
+        (None, "wrong_password", 422),
+        ("wrongemail@gmail.com", None, 422),
+    ],
+)
+def test_invalid_login(client, test_user, email, password, status_code):
+    res = client.post(
+        "/auth/login",
+        data={
+            "username": email,
+            "password": password,
+        },
+    )
+    assert res.status_code == status_code
