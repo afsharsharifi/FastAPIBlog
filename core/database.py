65523193e76b33fedcacfe937fabc07889639936
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-12-12 14:17:20 +0330

    Fixing Warnings for pydantic_settings & sqlalchmy

--- core/database.py
+++ core/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
