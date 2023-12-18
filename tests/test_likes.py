3fcf8b940c2b21065489c206f0411e4a06bc3393
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-12-18 12:53:31 +0330

    Tests for Removing Like From a Post

--- tests/test_likes.py
+++ tests/test_likes.py
@@ -23,14 +23,40 @@ def test_like_post_twice(authorized_client, test_posts, test_vote, test_user):
     assert res.json()["detail"] == f"User with id {test_user['id']} has already liked post with id {post_id}"
 
 
-def test_like_post_own_post(authorized_client, test_posts):
+def test_like_post_own(authorized_client, test_posts):
     post_id = test_posts[0].id
     res = authorized_client.post("vote/like", json={"post_id": post_id})
     assert res.status_code == 403
     assert res.json()["detail"] == f"Cant Like Own Post"
 
 
-def test_like_post_not_exists(authorized_client, test_posts):
+def test_like_post_not_exists(authorized_client):
     res = authorized_client.post("vote/like", json={"post_id": 0})
     assert res.status_code == 404
     assert res.json()["detail"] == f"Post with id 0 does not exists."
+
+
+def test_dislike_post_success(authorized_client, test_posts, test_vote):
+    post_id = test_posts[3].id
+    res = authorized_client.post("vote/dislike", json={"post_id": post_id})
+    assert res.status_code == 204
+
+
+def test_dislike_post_twice(authorized_client, test_posts):
+    post_id = test_posts[3].id
+    res = authorized_client.post("vote/dislike", json={"post_id": post_id})
+    assert res.status_code == 404
+    assert res.json()["detail"] == "User hasn't liked this post yet"
+
+
+def test_dislike_post_own(authorized_client, test_posts):
+    post_id = test_posts[0].id
+    res = authorized_client.post("vote/dislike", json={"post_id": post_id})
+    assert res.status_code == 403
+    assert res.json()["detail"] == "Cant Dislike Own Post"
+
+
+def test_dislike_post_not_exists(authorized_client, test_posts):
+    res = authorized_client.post("vote/dislike", json={"post_id": 0})
+    assert res.status_code == 404
+    assert res.json()["detail"] == "Post with id 0 does not exists."
